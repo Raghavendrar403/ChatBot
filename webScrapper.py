ed14191a1fe1d1a27d3c83fd93594d5fa1f94b1d
raghavendrar403 <Raghavendrar403@gmail.com>
2019-02-26 14:18:35 +0530

    created a new web scrapper to get popular places in a city

---
+++ webScrapper.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlopen as uReq
+from urllib.request import Request
+import os
+
+urlStart = "https://www.holidify.com/places/" 
+urlEnd = "/sightseeing-and-things-to-do.html"
+
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+
+with open('cities.txt') as cities:
+	citiesList = cities.read().split('\n')
+	citiesList.remove(citiesList[-1])
+
+places = citiesList
+
+dirName = "FamousPlaces"
+
+try:
+	os.mkdir(dirName)
+	print(f"Directory {dirName} created")
+except:
+	print(f"Directory {dirName} already exists")
+
+htmlDirName = "HTMLFiles"
+
+try:
+	os.mkdir(htmlDirName)
+	print(f"Directory {htmlDirName} created")
+except:
+	print(f"Directory {htmlDirName} already exists")
+
+
+# Start retrieving html files
+os.chdir(htmlDirName)
+for p in places:
+	# Complete URL to parse
+	myUrl = urlStart + p + urlEnd
+	print("================Started parsing "+p+"======================")
+	print("getting html for "+p)
+	req = Request(myUrl, headers=headers)
+	htmlObject = uReq(req)
+	htmlFile = htmlObject.read()
+	text = htmlFile.decode('utf-8')
+	print("saving the html file")
+	with open(p+'.html', 'w+') as myHTML:
+		myHTML.write(text)		
+	print('file '+myUrl+' saved');
+	
+os.chdir('..')
+
+
+#req = Request(myUrl,headers=headers)
+#
+#data = uReq(req)
+#
+#dataHTML = data.read()
+#
+#data.close()
+#
+## Start beautiful soup part
+#
+#dataSoup = bs(dataHTML, "html.parser")
+#
+#allh2 = dataSoup.findAll("h2",{"class":"ptvObjective"});
+#
+#famousPlaces = []
+#f = open("testBangalore.txt","w")
+#for x in allh2:
+#	print(x.text)
+#	famousPlaces.append(x.text)
+#	f.write("%s\n" % x.text);
+#
+#
+#f.close()
+#
+#
